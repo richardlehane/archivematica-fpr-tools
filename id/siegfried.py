e8e2115bbdc94e336bbe38da8c8e039525ffae39
Richard Lehane <richard.lehane@gmail.com>
2015-07-31 22:27:01 +1000

    Remove file call from siegfried fpr
    Siegfried 1.2 includes a text matcher based on file's text detection (see the github.com/richardlehane/characterize package).

--- id/siegfried.py
+++ id/siegfried.py
@@ -1,23 +1,15 @@
 from __future__ import print_function
  
 import socket
-import subprocess
 import sys
  
-def file_tool(path):
-    return subprocess.check_output(['file', path]).strip()
-    
 def main(path):
     sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     try:
         sock.connect('/tmp/siegfried')
         sock.sendall(path)
         reply = sock.recv(1024)
-        if reply.startswith('e'):
-            if reply.startswith('error: format unknown'):
-                if "text" in file_tool(path):
-                    print("x-fmt/111")
-                    return 0
+        if reply.startswith('error'):
             print("Siegfried sent {}.".format(reply), file=sys.stderr)
             return 1
         else:
